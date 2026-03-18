95d19c3dab231ef9572b1f4f5a91a4eb792e7141
Simon Dassow <simon@fabams.nl>
2026-03-18 19:01:42 +0100

    Add simple message source type based on a message list, and use to implement dummy message source

---
+++ widget/chat/message_source_dummy.go
@@ -0,0 +1,38 @@
+package chat
+
+import (
+	"fmt"
+	"time"
+)
+
+var dummyMessageTexts = []string{
+	"foo",
+	"bar",
+	"baz",
+	"lalala",
+	"moo",
+	"pew",
+	"mew",
+	"lalelu",
+	"woop",
+	"dee",
+	"doo",
+	"jajaja",
+	"meep",
+	"beep",
+	"woosh",
+}
+
+func NewDummyMessageSource(n int) *listMessageSource {
+	var msgs []Message
+	for i := 0; i < n; i++ {
+		msgs = append(msgs, NewMessage(
+			fmt.Sprintf("msg-%d", i+1),
+			"sndr-1",
+			"Dummy",
+			dummyMessageTexts[i%len(dummyMessageTexts)],
+			time.Date(2026, time.February, 1, 12, i, 0, 0, time.UTC),
+		))
+	}
+	return NewMessageSourceFromList(msgs)
+}
